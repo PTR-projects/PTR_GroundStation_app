07b62a58eade089c34ae481b2dd9bdc4d55bc733
Jan Olencki <jan@olencki.pl>
2024-08-31 11:50:25 +0200

    UI prototype

--- src/Views/MapContent.axaml.cs
+++ src/Views/MapContent.axaml.cs
@@ -7,5 +7,8 @@ public partial class MapContent : UserControl
     public MapContent()
     {
         InitializeComponent();
+        var mapControl = new Mapsui.UI.Avalonia.MapControl();
+        mapControl.Map?.Layers.Add(Mapsui.Tiling.OpenStreetMap.CreateTileLayer());
+        Content = mapControl;
     }
 }
